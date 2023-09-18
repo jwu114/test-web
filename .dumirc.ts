8324827e70e7e664e8e2ceb59fec8a412d1a1fef
Jerry Wu <91348682+jwu114@users.noreply.github.com>
2023-09-18 02:55:09 -0400

    initial commit

---
+++ .dumirc.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  themeConfig: {
+    name: 'test',
+  },
+  base: '/jwu114.github.io/',
+  publicPath: '/jwu114.github.io/',
+});
